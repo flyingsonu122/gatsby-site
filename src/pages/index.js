e559b152dd68c1dee5d02f6b5f6a1cf332fd427a
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-10-19 16:56:39 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>The Great Gatsby Bootcamp</h1>
+	  		<h2>I'm Sonu, a full-stack developer living in beautiful Bihar. </h2>
+        </div>
+    )
+}
+
+export default IndexPage
