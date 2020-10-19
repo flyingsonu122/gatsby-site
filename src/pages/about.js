46908eea41e91cc178327d2dce863f8116af42b1
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-10-19 17:16:48 +0530

    updated

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <h4>Hi there!</h4>
+            <h5> I'm Sonu kumar kushwaha from India, born in Gopalganj, a sunny place in the state of Bihar. A computer science student, Passionate reader. Eager to find the root cause of problems. For fun, I like to ride my motorcycle and follow my home cricket team.
+
+            </h5>
+        </div>
+    )
+}
+
+export default AboutPage
